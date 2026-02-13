3f49b207067015ddcf8bc4da766a96af2b4d27c7
Harun Ansari <harunansaru051@gmail.com>
2026-02-14 00:45:14 +0530

    Fixed NPE issue and improved dependency injection

--- src/main/java/com/mycomp/demoapp/controller/AdditionController.java
+++ src/main/java/com/mycomp/demoapp/controller/AdditionController.java
@@ -23,32 +23,14 @@ import lombok.extern.slf4j.Slf4j;
 public class AdditionController {
 	
 	// add slf4j logger object
-	private static final Logger logger = 
-			LoggerFactory.getLogger(AdditionController.class);
+	private static final Logger logger =
+		LoggerFactory.getLogger(AdditionController.class);
 
-
-			// add @Autowired for AdditionService
-			// field injection for AdditionService
-			
-			// add MyService and ApplicationContext as fields
+		// inject the addition service and my service using constructor injection
     private final AdditionService additionService;
-		private MyService myService;
+		private final MyService myService;
 		private final ApplicationContext applicationContext;
 	
-	// constructor injection for MyService and ApplicationContext
-	// public AdditionController( MyService myService, ApplicationContext applicationContext , AdditionService additionService) {
-		
-	// 	// log the injected dependencies
-	// 	logger.info("*** AdditionController initialized.");
-	// 	logger.info("MyService instance: {}", myService);
-	// 	logger.info("ApplicationContext instance: {}", applicationContext);
-	// 	logger.info("AdditionService instance: {}", additionService);
-
-	// 	// assign the injected dependencies to the class fields
-	// 	this.myService = myService;
-	// 	this.additionService = additionService;
-	// 	this.applicationContext = applicationContext;
-	// }
 
     @PostMapping
     public AdditionResponse add(@RequestBody AdditionRequest request) {
@@ -64,10 +46,6 @@ public class AdditionController {
 		for (String beanName : beanNames) {
 			//logger.info("Bean Name: {}", beanName);
 		}
-    	
-    	// dependency lookup
-    	myService = applicationContext.getBean(MyService.class);
-    	logger.info("Using Depedency Lookup Value MyService: {}", myService);
     	
     	String message = myService.myMethod();
 		logger.info("Message from MyService: {}", message);
---
+++ src/main/java/com/mycomp/demoapp/controller/tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+myService = applicationContext.getBean(MyService.class);
+    	// logger.info("Using Depedency Lookup Value MyService: {}", myService);
