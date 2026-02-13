501dae1fd8ce9d8e724b6b09dbbc652872f539d1
Harun Ansari <harunansaru051@gmail.com>
2026-02-14 02:18:35 +0530

    Gson code are written here in the Addition Service Impl

--- src/main/java/com/mycomp/demoapp/service/AdditionServiceImpl.java
+++ src/main/java/com/mycomp/demoapp/service/AdditionServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
+import com.google.gson.Gson;
 import com.mycomp.demoapp.pojo.AdditionRequest;
 import com.mycomp.demoapp.pojo.AdditionResponse;
 
@@ -16,14 +17,18 @@ import lombok.extern.slf4j.Slf4j;
 public class AdditionServiceImpl implements AdditionService {
    
     private final Random random ;
-	
+    private final Gson gson;
+
     @Override
     public AdditionResponse addNumbers(AdditionRequest request) {
 
         log.info("Inside AdditionServiceImpl.addNumbers() method with request: {}", request);
         int randomNum = random.nextInt(100);
         log.info("Generated random number: {}", randomNum);
-        
+
+        String requestJson = gson.toJson(request);
+        log.info("converted into AdditionRequest as JSON: {}", requestJson);
+
         int sum = request.getNum1() + request.getNum2() + randomNum;
         log.info("Calculated sum: {}", sum);
 
