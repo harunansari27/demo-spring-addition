c968582d4311fecb3568126bb9243996adf8dc8c
Harun Ansari <harunansaru051@gmail.com>
2026-02-14 02:33:13 +0530

    W2 D2 Complete Here

--- src/main/java/com/mycomp/demoapp/config/AppConfig.java
+++ src/main/java/com/mycomp/demoapp/config/AppConfig.java
@@ -5,6 +5,10 @@ import java.util.Random;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.gson.Gson;
+
+import com.google.gson.GsonBuilder;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
@@ -14,5 +18,12 @@ import lombok.extern.slf4j.Slf4j;
          Random random() {
            log.info("Creating Random bean");
            return new Random();
+     }
+
+
+     @Bean
+      Gson gson(GsonBuilder gsonBuilder) {
+         log.info("Creating Gson bean");
+         return gsonBuilder.create();
      }
 }
